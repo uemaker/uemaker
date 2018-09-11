db42f77dd61b9eaf8823510254226af6e65249cb
11253938 <hulianjia>
2018-09-11 17:42:51 +0800

    ss

--- manage/views/system.py
+++ manage/views/system.py
@@ -1,25 +1,59 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
+from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.shortcuts import render
 from manage.models import Module
 from manage.forms import ModuleAddForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import Q
+import json
+from django.http import HttpResponse
 
-class IndexView(View):
+class IndexView(TemplateView):
 
-    def get(self, request):
-        return render(request, 'manage/index.html', {})
+    template_name = 'manage/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
 
 class ModuleListView(ListView):
-    model = Module
-    context_object_name = "modules"
+    # model = Module
     template_name = 'manage/system/module_list.html'
+    context_object_name = "modules"
     paginate_by = 2
+
+    def post(self, request):
+        id_list = request.POST.getlist('id_list[]')
+        data = {}
+        if id_list:
+            idstring = ','.join(id_list)
+            Module.objects.extra(where=['id IN ('+ idstring +')']).delete()
+            data['success'] = 1
+        else:
+            data['success'] = 0
+
+        return HttpResponse(json.dumps(data), content_type="application/json")
+
+    def get_queryset(self):
+        listdata = Module.objects.all()
+        keyword = self.request.GET.get('q')
+        if keyword:
+            listdata = listdata.filter(Q(name__icontains=keyword)|Q(title__icontains=keyword))
+        return listdata
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ModuleListView, self).get_context_data(**kwargs)
+        context['form_url'] = reverse('modules')
+        context['q'] = self.request.GET.get('q', '')
+        return context
 
 def module_add(request):
     context = {}
     context['form_url'] = reverse('add_module')
+    context['form_title'] = '创建模块'
 
     if request.method == 'POST':
         form = ModuleAddForm(request.POST)
@@ -37,6 +71,7 @@ def module_edit(request, id):
     context = {}
     context['id'] = id
     context['form_url'] = reverse('edit_module', args=[id])
+    context['form_title'] = '编辑模块'
     model = Module.objects.filter(id=id).first()
 
     if request.method == 'POST':
---
+++ static/common/js/ueDialog.js
@@ -0,0 +1,162 @@
+;(function ($) {
+    $.ueDialog = function () {
+        var defaults = {
+            id: "modal",//弹窗id
+            title: "dialog",//弹窗标题
+            footer: true,
+            type: 'default',
+            showCancel: true,
+            backdrop: false,//是否显示遮障，和原生bootstrap 模态框一样
+            keyboard: true,//是否开启esc键退出，和原生bootstrap 模态框一样
+            remote: "",//加载远程url，和原生bootstrap 模态框一样
+            openEvent: null,//弹窗打开后回调函数
+            closeEvent: null,//弹窗关闭后回调函数
+            okEvent: null//单击确定按钮回调函数
+        };
+        //动态创建窗口
+        var _createDialog = {
+            init: function (opts) {
+                var _self = this;
+                var d = _self._html(opts);
+                $("body").append(d);
+                var modal = $("#" + opts.id);
+                //初始化窗口
+                modal.modal({
+                    backdrop: opts.backdrop,
+                    keyboard: opts.keyboard
+                });
+                if(opts.remote){
+                    $(".modal-body").load(opts.remote);
+                }
+
+                modal
+                //显示窗口
+                .modal('show')
+                //隐藏窗口后删除窗口html
+                .on('hidden.bs.modal', function () {
+                    console.log(3333);
+                    modal.remove();
+                    $(".modal-backdrop").remove();
+                    if (opts.closeEvent) {
+                        opts.closeEvent();
+                    }
+                })
+                //窗口显示后
+                .on('shown.bs.modal', function () {
+                    if (opts.openEvent) {
+                        opts.openEvent();
+                    }
+                });
+                //绑定按钮事件
+                $(".ok").click(function () {
+                    if (opts.okEvent) {
+                        opts.okEvent();
+                    }
+                    modal.modal('hide');
+                });
+
+                return modal;
+            },
+            _html: function (o) {
+                var model_cls = model_label_cls = model_dialog_cls = '';
+                if(o.type == 'small'){
+                    model_cls = 'bs-example-modal-sm';
+                    model_label_cls = 'mySmallModalLabel';
+                    model_dialog_cls = 'modal-sm';
+                }else if(o.type == 'large'){
+                    model_cls = 'bs-example-modal-lg';
+                    model_label_cls = 'myLargeModalLabel';
+                    model_dialog_cls = 'modal-lg';
+                }
+                modalHtml = '<div id="' + o.id + '" class="modal fade '+ model_cls +'" tabindex="-1" role="dialog" aria-labelledby="'+ model_label_cls +'">';
+                modalHtml += '<div class="modal-dialog '+ model_dialog_cls +'" role="document"><div class="modal-content">';
+                if(o.title){
+                    modalHtml += '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" ><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                    '<h4 id="myModalLabel" class="modal-title">' + o.title + '</h4></div>';
+                }
+                modalHtml += '<div class="modal-body" ><p>' + o.content + '</p></div>';
+                if(o.footer){
+                    modalHtml += '<div class="modal-footer">';
+                        if(o.showCancel){
+                            modalHtml += '<button class="btn" data-dismiss="modal" aria-hidden="true">取消</button>';
+                        }
+                    modalHtml += '<button class="btn btn-primary ok">确定</button></div>';
+                }
+                modalHtml += '</div></div></div>';
+                return modalHtml;
+            }
+        };
+        return {
+            success: function (msg, callback) {
+                var opt = $.extend({}, defaults, {
+                    id: 'success_modal',
+                    type: 'small',
+                    title: '',
+                    footer: false,
+                    content: msg,
+                    backdrop: true,
+                    keyboard: false,
+                    closeEvent: callback
+                });
+                $(".modal").remove();
+                var modal = _createDialog.init(opt);
+                setTimeout(function () {
+                    modal.modal('hide');
+                }, 2000)
+            },
+            error: function (msg) {
+                var opt = $.extend({}, defaults, {
+                    id: 'error_modal',
+                    type: 'small',
+                    title: '',
+                    footer: false,
+                    content: msg,
+                    backdrop: true,
+                    keyboard: false
+                });
+                $(".modal").remove();
+                var modal = _createDialog.init(opt);
+                setTimeout(function () {
+                    modal.modal('hide');
+                }, 2000)
+            },
+            alert: function (msg) {
+                var opt = $.extend({}, defaults, {
+                    id: 'alert_modal',
+                    type: 'small',
+                    title: '',
+                    footer: true,
+                    content: msg,
+                    backdrop: true,
+                    keyboard: false,
+                    showCancel: false,
+                });
+                $(".modal").remove();
+                _createDialog.init(opt);
+            },
+            confirm: function (msg, callback) {
+                 var opt = $.extend({}, defaults, {
+                    id: 'confirm_modal',
+                    type: 'small',
+                    title: '',
+                    footer: true,
+                    content: msg,
+                    backdrop: true,
+                    keyboard: false,
+                    okEvent: callback
+                });
+                $(".modal").remove();
+                _createDialog.init(opt);
+            },
+            loadpage: function (url, opt) {
+                var opt = $.extend({}, defaults, {
+                    id: 'page_modal',
+                    remote: url
+                }, opt);
+                $(".modal").remove();
+                _createDialog.init(opt);
+            }
+        };
+
+    }
+})(jQuery);
